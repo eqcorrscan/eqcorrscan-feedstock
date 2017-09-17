d02aadf79927304bc573bbde09a3a539c113629f
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2017-09-17 17:41:19 +1200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@
 
 set "INCLUDE=%LIBRARY_INC%;%INCLUDE%"
 set "LIB=%LIBRARY_LIB%;%LIB%"
-copy %LIBRARY_LIB%\fftw3.lib %LIBRARY_LIB%\libfftw3-3.lib
+copy %LIBRARY_LIB%\fftw3.lib %LIBRARY_LIB%\libfftw3-3.lib %LIBRARY_LIB%\libfftw3f-3.lib %LIBRARY_LIB%\fftw3f.lib
 
 %PYTHON% setup.py build
 %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
