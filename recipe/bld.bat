6426c8959060f644b71b3ae1e49b19737c5a5771
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2020-06-23 11:44:56 +1200

    STATIC-FFTW

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,9 @@
+
 set C_INCLUDE_PATH="%PREFIX%\include"
 set STATIC_FFTW_DIR="%PREFIX%\lib"
+
+set "INCLUDE=%LIBRARY_INC%;%INCLUDE%"
+set "LIB=%LIBRARY_LIB%;%LIB%"
 copy %LIBRARY_LIB%\fftw3.lib %LIBRARY_LIB%\libfftw3-3.lib
 copy %LIBRARY_LIB%\fftw3f.lib %LIBRARY_LIB%\libfftw3f-3.lib
 
