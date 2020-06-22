4a3485a68b557d29dea16a61e5ec335714d766c6
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2020-06-23 11:57:09 +1200

    STATIC-FFTW

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,17 @@
+echo on
 
 set C_INCLUDE_PATH="%PREFIX%\include"
-set STATIC_FFTW_DIR="%PREFIX%\lib"
+set STATIC_FFTW_DIR="%LIBRARY_LIB%\lib"
 
 set "INCLUDE=%LIBRARY_INC%;%INCLUDE%"
 set "LIB=%LIBRARY_LIB%;%LIB%"
 copy %LIBRARY_LIB%\fftw3.lib %LIBRARY_LIB%\libfftw3-3.lib
 copy %LIBRARY_LIB%\fftw3f.lib %LIBRARY_LIB%\libfftw3f-3.lib
+
+echo %C_INCLUDE_PATH%
+echo %STATIC_FFTW_DIR%
+echo %LIB%
+echo %INCLUDE%
 
 %PYTHON% setup.py build
 %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
