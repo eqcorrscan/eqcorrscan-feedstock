12b4c13112ec977bbdb058cfe875e8de3b5036cc
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2020-07-01 09:57:47 +1200

    Try static linking.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,5 +5,7 @@ set "LIB=%LIBRARY_LIB%;%LIB%"
 copy %LIBRARY_LIB%\fftw3.lib %LIBRARY_LIB%\libfftw3-3.lib
 copy %LIBRARY_LIB%\fftw3f.lib %LIBRARY_LIB%\libfftw3f-3.lib
 
+set "STATIC_FFTW_DIR=%PREFIX%/lib"
+
 %PYTHON% setup.py build
 %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
