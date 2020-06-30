f80c33106caf758202745e87a55bcd2453d65e12
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2020-07-01 10:53:36 +1200

    This is such a waste of time - hopefully this skips windows 3.8

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,5 @@ set "LIB=%LIBRARY_LIB%;%LIB%"
 copy %LIBRARY_LIB%\fftw3.lib %PREFIX%\lib\libfftw3-3.lib
 copy %LIBRARY_LIB%\fftw3f.lib %PREFIX%\lib\libfftw3f-3.lib
 
-set "STATIC_FFTW_DIR=%PREFIX%/lib"
-
 %PYTHON% setup.py build
 %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
